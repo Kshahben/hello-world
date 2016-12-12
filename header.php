4a7ace44613dbc234defa669095b510ae3e73f9d
lisaauquier <lisaauquier@hotmail.com>
2016-12-12 12:16:53 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bullet Journal</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../style/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../style/style.css">
+		
+	<link href="https://fonts.googleapis.com/css?family=Codystar:300" rel="stylesheet">
+</head>
+<body>
+		<h1>Bullet Journal</h1>
+		<h6>Adapted with the Minimalist Philosophy of Leo Babauta</h6>
+
+		<!--<div id="menu">
+			<ul>
+				<li><a href="index.php?page=accueil">HOMEPAGE</a></li>
+				<li><a href="index.php?page=name">NAME</a></li>
+				<li><a href="index.php?page=tva">TVA</a></li>
+				<li><a href="index.php?page=switch">SWITCH</a></li>
+				<li><a href="index.php?page=tableau">TABLEAU</a></li>
+				<li><a href="index.php?page=while">WHILE</a></li>
+				<li><a href="index.php?page=chainedecarac">CHAINE DE CARACTÈRES</a></li>
+			</ul>
+		</div>-->
+
+	
